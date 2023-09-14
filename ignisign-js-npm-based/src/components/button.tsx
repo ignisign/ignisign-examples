90e1c1234a8dc77a2a7b96c4e1f9d9ea350501b1
Loic-Fritsch <loic.fritsch.fr.com>
2023-09-14 15:51:34 +0200

    fix

--- ignisign-js-npm-based/src/components/button.tsx
+++ ignisign-js-npm-based/src/components/button.tsx
@@ -22,7 +22,9 @@ const Button = ({
   return (
     <div className='relative'> 
       { loading && 
-        <LoadingSpinner className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/> 
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+          <LoadingSpinner/> 
+        </div>
       }
 
       <MuiButton
